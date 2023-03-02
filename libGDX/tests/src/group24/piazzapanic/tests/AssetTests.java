3dc0dc641a8f71505521bdb7ca4886521c938ae0
joshuamckean <joshuamckean1@gmail.com>
2023-03-02 01:44:03 +0000

    Added stuff for tests [WIP]
    This still does not work, the issue is that it cannot find the files from the test, probably an issue with gradle

---
+++ libGDX/tests/src/group24/piazzapanic/tests/AssetTests.java
@@ -0,0 +1,18 @@
+package group24.piazzapanic.tests;
+
+import com.badlogic.gdx.Gdx;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(GdxTestRunner.class)
+public class AssetTests {
+
+    @Test
+    public void testFloorAssetExists() {
+        assertTrue("This test will only pass if the floor asset exists",
+                Gdx.files.internal("kitchen_floor.png").exists());
+    }
+
+}
