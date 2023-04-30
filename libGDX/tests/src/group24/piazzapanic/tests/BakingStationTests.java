7b94c5a40e9dbf5f05bc3e659de06358b99cab9e
seifhussein11 <smkh500@york.ac.uk>
2023-04-30 01:44:28 +0100

    bakingStation test update

--- libGDX/tests/src/group24/piazzapanic/tests/BakingStationTests.java
+++ libGDX/tests/src/group24/piazzapanic/tests/BakingStationTests.java
@@ -1,7 +1,9 @@
 package group24.piazzapanic.tests;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import group24.piazzapanic.game.GameData;
+import group24.piazzapanic.game.GameLoop;
 import group24.piazzapanic.levelElements.Ingredient;
 import group24.piazzapanic.levelElements.IngredientType;
 import group24.piazzapanic.levelElements.stations.BakingStation;
@@ -21,7 +23,9 @@ public class BakingStationTests {
 
         ByteArrayOutputStream output1 = new ByteArrayOutputStream();
         System.setOut(new PrintStream(output1));
+        GameLoop gameLoop = Mockito.mock(GameLoop.class);
         GameData gameData = Mockito.mock(GameData.class);
+        gameData.gameLoop = gameLoop;
         Texture texture = Mockito.mock(Texture.class);
         gameData.bakingStationTexture = texture;
         Ingredient item = Mockito.mock(Ingredient.class);
@@ -48,6 +52,41 @@ public class BakingStationTests {
         station.interact(1);
         assertEquals(1, item2.getBakingProgress(),0.0000001);
         assertEquals(0,station.timeKeyHeld,0.0000001);
+        station.available=0;
+        gameData.money = 3;
+        Integer expectedMoneyResult = gameData.money - station.cost;
+        gameData.gameLoop.bakingStationPrice = Mockito.mock(Label.class);
+        ByteArrayOutputStream output4 = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output4));
+        station.interact(1);
+        assertEquals(expectedMoneyResult,gameData.money);
+        assertEquals(1,station.available);
+        assertEquals("Disabled"+System.lineSeparator(), output4.toString());
+        station.available=0;
+        gameData.gameLoop.bakingStationPrice = Mockito.mock(Label.class);
+        ByteArrayOutputStream output5 = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output5));
+        station.interact(1);
+        assertEquals("Disabled"+System.lineSeparator(), output5.toString());
+        station.available=1;
+        Ingredient item3 = new Ingredient(bread,-1,3,-1);
+        station.item = item3;
+        float timeHeld = station.timeKeyHeld;
+        station.interact(1);
+        assertEquals(3,item3.getBakingProgress(),0.0000001);
+        assertEquals(timeHeld + 1,station.timeKeyHeld,0.0000001);
+        station.item = item3;
+        station.timeKeyHeld=1;
+
+        station.interact(1);
+        assertEquals(2,station.timeKeyHeld,0.0000001);
+        station.item=item3;
+        station.timeKeyHeld=3;
+        station.interact(1);
+        assertEquals(4,station.timeKeyHeld,0.0000001);
+
+
+
 
 
 
